5a1238b047efdf1df3b56897a1446f672b43da9a
Evangelos Voultepsis <33362678+evangelosv93@users.noreply.github.com>
2018-04-09 23:29:26 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+let correctGuesses = 0;
+let bestScore = 0;
+let clickMessage = "Click on an image to earn points, but don't click on any of them more than once!";
+
+//need to code if pic is clicked more than once in a row

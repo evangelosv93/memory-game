0b6996b1ea402ddaa427f4a814f8dcdc31c373b3
Evangelos Voultepsis <33362678+evangelosv93@users.noreply.github.com>
2018-04-09 23:07:55 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var router = express.Router();
+var bodyParser = require("body-parser");
+var path = require("path");
+var port = process.env.PORT || 8080;
+var app = express();
